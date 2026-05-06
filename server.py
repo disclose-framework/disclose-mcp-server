e71b57684270b8d140879789e27d5b60c92f63f7
danielwhitefield <danielwhitefield@gmail.com>
2026-05-06 14:14:41 -0400

    server.py

--- server.py
+++ server.py
@@ -11,6 +11,7 @@ from starlette.routing import Route, Mount
 
 mcp = FastMCP(
     "Disclose",
+    icon="https://discloseframework.dev/disclose_protocol_logo.jpg",
     stateless_http=True,
     transport_security=TransportSecuritySettings(
         enable_dns_rebinding_protection=False,
